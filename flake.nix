20bbb2512dfdf7c5656f086d511e4162b822648e
YZTangent <yuanzheng.tan.tyz@gmail.com>
2025-09-21 16:47:39 +0800

    chore: set up dev env with flakes

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "techjam";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+
+      pythonEnv = pkgs.python3.withPackages (ps:
+        with ps; [
+          requests
+          pydantic
+          pydantic
+          jsonschema
+          python-dateutil
+          typing-extensions
+          colorama
+          tqdm
+
+          numpy
+          pandas
+          matplotlib
+          scikit-image
+
+          uv
+        ]);
+      commonPackages = with pkgs; [
+        # libGL glibc glib 
+        pythonEnv
+        ollama
+      ];
+
+    in {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+
+        packages = commonPackages;
+
+        shellHook = ''
+          echo "Starting Ollama..."
+          ollama serve &
+
+          sleep 2
+
+          echo "Ollama started successfully."
+          echo "Entering Nix shell..."
+
+          cleanup_ollama_on_exit() {
+            echo "Exiting Nix shell. Running cleanup..."
+            pkill ollama
+            echo "Cleanup complete."
+          }
+
+          # Trap the EXIT signal to call the cleanup function
+          trap cleanup_ollama_on_exit EXIT
+        '';
+
+        # env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+        #   pkgs.stdenv.cc.cc.lib
+        #   pkgs.libz
+        #   pkgs.libGL
+        #   pkgs.glibc
+        #   pkgs.glib
+        # ];
+      };
+    };
+}
