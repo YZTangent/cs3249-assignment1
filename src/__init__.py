fb3c2817f48bcd95c835ff696557ee8d73770983
YZTangent <yuanzheng.tan.tyz@gmail.com>
2025-09-21 22:15:32 +0800

    chore: add name and student number

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,6 @@ Psychological Pre-Consultation CUI System
 2025S1 CS3249 Assignment 1
 """
 
-__studentName__ = "Your Full Name Here"
-__nusId__ = "Your NUS ID Here"
+__studentName__ = "Tan Yuanzheng"
+__nusId__ = "A0235393B"
+
